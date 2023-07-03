c4da89fd6c1318ccbc9a7f8ac7f4a00fa79a40e8
aga87 <aga.labonarska@outlook.com>
2023-07-03 20:50:51 +0200

    Set up a separate database for testing

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ import { logger } from './startup/logger';
 import { routes } from './startup/routes';
 
 dotenv.config();
-const app: Application = express();
+export const app: Application = express();
 
 process.on('uncaughtException', (ex: Error) => {
   logger.error(ex.message, ex);
@@ -21,13 +21,16 @@ process.on('unhandledRejection', (ex: Error) => {
 
 routes(app);
 
-const { PORT, MONGO_DB_URI } = process.env;
+if (process.env.NODE_ENV !== 'test') {
+  const { MONGO_DB_URI } = process.env;
 
-if (!MONGO_DB_URI) {
-  throw new Error('FATAL ERROR: MONGO_DB_URI is not defined.');
+  if (!MONGO_DB_URI) {
+    throw new Error('FATAL ERROR: MONGO_DB_URI is not defined.');
+  }
+
+  connectToMongoDB(MONGO_DB_URI);
 }
 
-connectToMongoDB(MONGO_DB_URI);
-
+const { PORT } = process.env;
 const port = PORT || 5000;
 app.listen(port, () => logger.info(`Listening on port ${port}...`));
