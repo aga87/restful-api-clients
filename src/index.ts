523687d26cba077596eb9bd14e11c2a59c52383e
aga87 <aga.labonarska@outlook.com>
2023-07-03 20:52:50 +0200

    Listen on port 0 when running tests

--- src/index.ts
+++ src/index.ts
@@ -22,15 +22,14 @@ process.on('unhandledRejection', (ex: Error) => {
 routes(app);
 
 if (process.env.NODE_ENV !== 'test') {
-  const { MONGO_DB_URI } = process.env;
+  const { MONGO_DB_URI, PORT } = process.env;
 
   if (!MONGO_DB_URI) {
     throw new Error('FATAL ERROR: MONGO_DB_URI is not defined.');
   }
 
   connectToMongoDB(MONGO_DB_URI);
-}
 
-const { PORT } = process.env;
-const port = PORT || 5000;
-app.listen(port, () => logger.info(`Listening on port ${port}...`));
+  const port = PORT || 5000;
+  app.listen(port, () => logger.info(`Listening on port ${port}...`));
+}
