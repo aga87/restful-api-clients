b1efcffbdfbb0ca39300a65bdeac4d0ef3fa8d86
aga87 <aga.labonarska@outlook.com>
2023-07-04 14:47:38 +0200

    Implement pre-save middleware to encrypt (and trim) data

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,4 +1,5 @@
 import { Schema, model } from 'mongoose';
+import { EncryptionService } from '../services/encryption';
 import type { ClientRecord } from '../types/types';
 
 const clientSchema = new Schema<ClientRecord>({
@@ -16,6 +17,20 @@ const clientSchema = new Schema<ClientRecord>({
       required: true
     }
   }
+});
+
+// Pre-save middleware
+clientSchema.pre<ClientRecord>('save', function (next) {
+  const client = this;
+
+  // Trim and encrypt the firstName, lastName, and address.postalCode fields
+  client.firstName = EncryptionService.encryptData(client.firstName.trim());
+  client.lastName = EncryptionService.encryptData(client.lastName.trim());
+  client.address.postalCode = EncryptionService.encryptData(
+    client.address.postalCode.trim()
+  );
+
+  next();
 });
 
 export const Client = model<ClientRecord>('Client', clientSchema);
